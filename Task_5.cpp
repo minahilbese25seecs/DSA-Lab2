718c7def4618aa4f3bd5d784a14537299c535d2d
Minahil Tahir <minutahir494@gmail.com>
2026-09-18 22:43:17 +0500

    Fixed assert errors

--- Task_5.cpp
+++ Task_5.cpp
@@ -16,21 +16,21 @@ void testSortedArray() {
 int arr[] = {1, 2, 3, 4, 5};
 bool result = IsSorted(arr, 5);
 assert(result == true);
-cout<<"Sorted Array test passed.";
+cout<<"Sorted Array test passed.\n";
 }
 
 void testUnsortedArray() { 
 int arr[] = {5,8,7,3,1};
 bool result = IsSorted(arr, 5);
 assert(result == false);
-cout<<"UnSorted Array test passed.";
+cout<<"UnSorted Array test passed.\n";
 }
 
 void testDuplicatedArray() { 
-int arr[] = {1, 2, 3, 4, 5};
+int arr[] = {1, 2, 2, 4, 5};
 bool result = IsSorted(arr, 5);
 assert(result == true);
-cout<<"Duplicated Array test passed.";
+cout<<"Duplicated Array test passed.\n";
 }
 
 
@@ -38,34 +38,34 @@ void testSingleElement() {
 int arr[] = {1};
 bool result = IsSorted(arr, 1);
 assert(result == true);
-cout<<"Single Element test passed.";
+cout<<"Single Element test passed.\n";
 }
 
 
 void testDescendingArray() { 
 int arr[] = {5,4,3,2,1};
 bool result = IsSorted(arr, 5);
-assert(result == true);
-cout<<"Descending Array test passed.";
+assert(result == false);
+cout<<"Descending Array test passed.\n";
 }
 
 
 void testNegativeArray() { int arr[] = {-3,-2,-1,-9,-8};
 bool result = IsSorted(arr, 5);
-assert(result == true);
-cout<<"Negative Array test passed.";
+assert(result == false);
+cout<<"Negative Array test passed.\n";
 }
 
 void testEmptyArray() { int arr[] = {};
 bool result = IsSorted(arr, 0);
 assert(result == true);
-cout<<"Empty Array test passed.";
+cout<<"Empty Array test passed.\n";
 }
 
 void testEqualArray() { int arr[] = {1,1,1};
 bool result = IsSorted(arr, 3);
 assert(result == true);
-cout<<"Equal Array test passed.";
+cout<<"Equal Array test passed.\n";
 }
 int main(){  
 
@@ -78,6 +78,6 @@ int main(){
     testEqualArray();
     testEmptyArray(); 
     
-    cout<<"All tests passed";
+    
 
 }
